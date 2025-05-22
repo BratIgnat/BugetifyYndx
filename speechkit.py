ceab9f7fef6e27bf8cadafa57627213a7b641c82
BratIgnat <86632889+BratIgnat@users.noreply.github.com>
2025-05-23 02:37:24 +0300

    Update speechkit.py

--- speechkit.py
+++ speechkit.py
@@ -1,7 +1,7 @@
-import requests
 import os
-from pydub import AudioSegment
+import requests
 from tempfile import NamedTemporaryFile
+from pydub import AudioSegment
 
 YANDEX_API_KEY = os.getenv("YANDEX_API_KEY")
 
@@ -9,22 +9,20 @@ def speech_to_text(ogg_data):
     with NamedTemporaryFile(suffix=".ogg", delete=True) as ogg_file:
         ogg_file.write(ogg_data)
         ogg_file.flush()
+
         audio = AudioSegment.from_ogg(ogg_file.name)
 
         with NamedTemporaryFile(suffix=".wav", delete=True) as wav_file:
             audio.export(wav_file.name, format="wav")
             wav_file.flush()
+
             with open(wav_file.name, "rb") as f:
                 audio_bytes = f.read()
 
-    headers = {
-        "Authorization": f"Api-Key {YANDEX_API_KEY}",
-    }
-    response = requests.post(
-        "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize",
-        headers=headers,
-        params={"lang": "ru-RU"},
-        data=audio_bytes,
-    )
+    url = "https://stt.api.cloud.yandex.net/speech/v1/stt:recognize"
+    headers = {"Authorization": f"Api-Key {YANDEX_API_KEY}"}
+    params = {"lang": "ru-RU"}
+
+    response = requests.post(url, headers=headers, params=params, data=audio_bytes)
     response.raise_for_status()
     return response.json().get("result")
